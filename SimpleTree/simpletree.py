da058e5dd43fac31bef2bde868ef2c1e7c6430ca
makrotheeast <makrotheeast@gmail.com>
2023-03-15 00:40:56 +0300

    Started working on ADS-2

---
+++ SimpleTree/simpletree.py
@@ -0,0 +1,74 @@
+class SimpleTreeNode:
+
+    def __init__(self, val, parent=None):
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.Children = []  # список дочерних узлов
+
+
+class SimpleTree:
+
+    def __init__(self, root: SimpleTreeNode):
+        self.Root = root  # корень, может быть None
+        self.nodes = [root]
+
+    def _get_all_child(self, parent_node):
+        if len(parent_node.Children) == 0:
+            return []
+
+        child_nodes = parent_node.Children
+
+        for node in child_nodes:
+            child_nodes = child_nodes + self._get_all_child(node)
+            # Это не работает и я не понимаю, почему
+            # child_nodes += self._get_all_child(node)
+
+        return child_nodes
+
+
+
+    def AddChild(self, ParentNode: SimpleTreeNode, NewChild: SimpleTreeNode):
+        if not isinstance(ParentNode, SimpleTreeNode):
+            raise ValueError('ParentNode isn\'t SimpleTreeNode object.')
+        if not isinstance(NewChild, SimpleTreeNode):
+            raise ValueError('NewChild isn\'t SimpleTreeNode object.')
+        if ParentNode not in self.nodes:
+            pass #raise ValueError('ParentNode doesn\'t belong to tree.')
+
+        ParentNode.Children.append(NewChild)
+        NewChild.Parent = ParentNode
+        self.nodes.append(NewChild)
+
+    def DeleteNode(self, NodeToDelete):
+        if NodeToDelete is self.Root:
+            return
+        
+        # Нужно получить ссылки на все дочерние узлы и применить к ним ко всем
+        # оператор del
+
+
+
+
+
+        pass  # ваш код удаления существующего узла NodeToDelete
+
+    def GetAllNodes(self):
+        # ваш код выдачи всех узлов дерева в определённом порядке
+        return []
+
+    def FindNodesByValue(self, val):
+        # ваш код поиска узлов по значению
+        return []
+
+    def MoveNode(self, OriginalNode, NewParent):
+        # ваш код перемещения узла вместе с его поддеревом --
+        # в качестве дочернего для узла NewParent
+        pass
+
+    def Count(self):
+        # количество всех узлов в дереве
+        return 0
+
+    def LeafCount(self):
+        # количество листьев в дереве
+        return 0
---
+++ SimpleTree/test_simpletree.py
@@ -0,0 +1,123 @@
+from simpletree import SimpleTree, SimpleTreeNode
+
+
+def create_tree_for_tests():
+    pass
+
+
+def test_create_simpletree():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    assert tree.Root is root
+    assert root.Parent is None
+    assert root.NodeValue == 1
+
+
+def test_add_level_1_child():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    child_1_lvl_1 = SimpleTreeNode(1)
+    child_2_lvl_1 = SimpleTreeNode(1)
+
+    tree.AddChild(root, child_1_lvl_1)
+    tree.AddChild(root, child_2_lvl_1)
+
+    assert child_1_lvl_1 in tree.Root.Children
+    assert child_2_lvl_1 in tree.Root.Children
+
+    assert child_1_lvl_1.Parent is root
+    assert child_2_lvl_1.Parent is root
+
+
+def test_add_level_2_child():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    child_lvl_1 = SimpleTreeNode(1)
+    child_lvl_2 = SimpleTreeNode(1)
+
+    tree.AddChild(root, child_lvl_1)
+    tree.AddChild(child_lvl_1, child_lvl_2)
+
+    assert child_lvl_1.Parent is root
+    assert child_lvl_2.Parent is child_lvl_1
+
+
+def test_get_children_empty():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    assert len(tree._get_all_child(root)) == 0
+
+
+def test_get_children_normal():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    child_1_lvl_1 = SimpleTreeNode(1)
+    child_1_lvl_2 = SimpleTreeNode(1)
+
+    tree.AddChild(root, child_1_lvl_1)
+    tree.AddChild(child_1_lvl_1, child_1_lvl_2)
+
+    assert len(tree._get_all_child(root)) == 2
+
+
+def test_get_children_bigger():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    child_1_lvl_1 = SimpleTreeNode(1)
+    child_2_lvl_1 = SimpleTreeNode(1)
+    child_1_lvl_2 = SimpleTreeNode(1)
+    child_2_lvl_2 = SimpleTreeNode(1)
+    child_3_lvl_2 = SimpleTreeNode(1)
+    child_1_lvl_3 = SimpleTreeNode(1)
+
+    tree.AddChild(root, child_1_lvl_1)
+    tree.AddChild(root, child_2_lvl_1)
+    tree.AddChild(child_1_lvl_1, child_1_lvl_2)
+    tree.AddChild(child_1_lvl_1, child_2_lvl_2)
+    tree.AddChild(child_1_lvl_1, child_3_lvl_2)
+    tree.AddChild(child_1_lvl_2, child_1_lvl_3)
+
+    founded_nodes = tree._get_all_child(root)
+    assert set(founded_nodes).discard({child_1_lvl_1, child_2_lvl_1, child_1_lvl_2, child_2_lvl_2,
+                                       child_3_lvl_2, child_1_lvl_3}) is None
+
+
+def test_get_children_of_children():
+    root = SimpleTreeNode(1)
+    tree = SimpleTree(root)
+
+    child_1_lvl_1 = SimpleTreeNode(1)
+    child_2_lvl_1 = SimpleTreeNode(1)
+    child_1_lvl_2 = SimpleTreeNode(1)
+    child_2_lvl_2 = SimpleTreeNode(1)
+    child_3_lvl_2 = SimpleTreeNode(1)
+    child_1_lvl_3 = SimpleTreeNode(1)
+
+    tree.AddChild(root, child_1_lvl_1)
+    tree.AddChild(root, child_2_lvl_1)
+    tree.AddChild(child_1_lvl_1, child_1_lvl_2)
+    tree.AddChild(child_1_lvl_1, child_2_lvl_2)
+    tree.AddChild(child_1_lvl_1, child_3_lvl_2)
+    tree.AddChild(child_1_lvl_2, child_1_lvl_3)
+
+    founded_nodes = tree._get_all_child(child_1_lvl_1)
+    assert set(founded_nodes).discard({child_1_lvl_2, child_2_lvl_2, child_3_lvl_2,
+                                       child_1_lvl_3}) is None
+
+
+# def test_delete():
+#     root = SimpleTreeNode(5, None)
+#     tree = SimpleTree(root)
+
+#     child_lvl_1 = SimpleTreeNode(1)
+#     child_lvl_2 = SimpleTreeNode(1)
+
+#     tree.AddChild(root, child_lvl_1)
+#     tree.AddChild(child_lvl_1, child_lvl_2)
+
