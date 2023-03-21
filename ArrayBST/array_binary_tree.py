e1bd2563080d3ebf41603dd04786c7df4347de63
Denis <makrotheeast@gmail.com>
2023-03-21 22:29:04 +0300

    Have ended working on lesson 4

---
+++ ArrayBST/array_binary_tree.py
@@ -0,0 +1,58 @@
+class aBST:
+
+    def __init__(self, depth):
+        # правильно рассчитайте размер массива для дерева глубины depth:
+        tree_size = 2 ** (depth + 1) - 1
+        self.Tree = [None] * tree_size  # массив ключей
+
+    def FindKeyIndex(self, key):
+        if self.Tree[0] is None:
+            return None
+
+        return self._find(0, key)
+
+    def _find(self, index, key):
+        # Поздновато я принял во внимание, что метод поиска должен
+        # возвращать индекс подходящего места для вставки ключа,
+        # умноженный на -1. Алгоритм добавления ключа был реализован
+        # раньше и мне жалко его удалять, чтобы заменить его на вызов
+        # метода поиска
+
+        if self.Tree[index] is None:
+            return index * -1
+
+        if self.Tree[index] == key:
+            return index
+
+        left_child_index = 2 * index + 1
+        if left_child_index >= len(self.Tree):
+            return None
+        if key < self.Tree[index]:
+            return self._find(left_child_index, key)
+
+        right_child_index = 2 * index + 2
+        if (2 * index + 2) >= len(self.Tree):
+            return None
+        if key > self.Tree[index]:
+            return self._find(right_child_index, key)
+
+    def AddKey(self, key):
+        # индекс добавленного/существующего ключа или -1 если не удалось
+        return self._add(0, key)
+
+    def _add(self, index, key):
+        if self.Tree[index] is None:
+            self.Tree[index] = key
+            return index
+
+        left_child_index = 2 * index + 1
+        if left_child_index >= len(self.Tree):
+            return -1
+        if key < self.Tree[index]:
+            return self._add(left_child_index, key)
+
+        right_child_index = 2 * index + 2
+        if (2 * index + 2) >= len(self.Tree):
+            return -1
+        if key > self.Tree[index]:
+            return self._add(right_child_index, key)
---
+++ ArrayBST/test_array_binary_tree.py
@@ -0,0 +1,78 @@
+from array_binary_tree import *
+
+
+# Test Add
+
+def test_add_1():
+    tree = aBST(0)
+    tree.AddKey(5)
+
+    assert tree.Tree == [5]
+
+
+def test_add_2():
+    tree = aBST(2)
+    tree.AddKey(10)
+    tree.AddKey(5)
+    tree.AddKey(15)
+    tree.AddKey(2)
+    tree.AddKey(7)
+    tree.AddKey(12)
+    tree.AddKey(17)
+
+    assert tree.Tree == [10, 5, 15, 2, 7, 12, 17]
+
+
+def test_add_3():
+    tree = aBST(3)
+    assert tree.AddKey(50) == 0
+    tree.AddKey(25)
+    assert tree.AddKey(75) == 2
+    tree.AddKey(37)
+    assert tree.AddKey(62) == 5
+    tree.AddKey(84)
+    assert tree.AddKey(31) == 9
+    tree.AddKey(43)
+    assert tree.AddKey(55) == 11
+    tree.AddKey(92)
+
+    assert tree.Tree == [50, 25, 75, None, 37, 62,
+                         84, None, None, 31, 43, 55, None, None, 92]
+
+
+# Test Find
+
+def test_find_1():
+    tree = aBST(0)
+    tree.AddKey(5)
+
+    assert tree.FindKeyIndex(5) == 0
+    assert tree.FindKeyIndex(1) is None
+
+
+def test_find_2():
+    tree = aBST(3)
+    tree.AddKey(50)
+    tree.AddKey(25)
+    tree.AddKey(75)
+    tree.AddKey(37)
+    tree.AddKey(62)
+    tree.AddKey(84)
+    tree.AddKey(31)
+    tree.AddKey(43)
+    tree.AddKey(55)
+    tree.AddKey(92)
+
+    assert tree.FindKeyIndex(50) == 0
+    assert tree.FindKeyIndex(25) == 1
+    assert tree.FindKeyIndex(75) == 2
+    assert tree.FindKeyIndex(37) == 4
+    assert tree.FindKeyIndex(62) == 5
+    assert tree.FindKeyIndex(84) == 6
+    assert tree.FindKeyIndex(31) == 9
+    assert tree.FindKeyIndex(43) == 10
+    assert tree.FindKeyIndex(55) == 11
+    assert tree.FindKeyIndex(92) == 14
+
+    assert tree.FindKeyIndex(999) is None
+    assert tree.FindKeyIndex(-999) == -3
