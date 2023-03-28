243fa3f35fafbcd68aeede73671bcdab07f91ece
makrotheeast <makrotheeast@gmail.com>
2023-03-28 15:25:59 +0300

    Have added additional test

--- DFSGraph/test_dfs_graph.py
+++ DFSGraph/test_dfs_graph.py
@@ -125,3 +125,24 @@ def test_dfs_5():
     g.AddEdge(1, 3)
 
     assert g.DepthFirstSearch(0, 3) == [g.vertex[0], g.vertex[1], g.vertex[3]]
+
+
+def test_dfs_addotional():
+    g = SimpleGraph(5)
+
+    g.AddVertex(0)  # A
+    g.AddVertex(1)  # B
+    g.AddVertex(2)  # C
+    g.AddVertex(3)  # D
+    g.AddVertex(4)  # E
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 3)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 3)
+    g.AddEdge(2, 3)
+    g.AddEdge(1, 4)
+    g.AddEdge(3, 4)
+    g.AddEdge(3, 3)
+
+    assert g.DepthFirstSearch(0, 4) == [g.vertex[0], g.vertex[1], g.vertex[4]]
