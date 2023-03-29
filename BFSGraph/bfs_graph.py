6e4e05b1aaa66d7803157fc1b5fd0f21a102ccf5
YonderMeizster <makrotheeast@gmail.com>
2023-03-29 21:29:09 +0300

    Have started working on lesson 11

---
+++ BFSGraph/bfs_graph.py
@@ -0,0 +1,179 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+        self.Hit = False
+
+    def __repr__(self) -> str:
+        return str(self.Value)
+
+
+class SimpleGraph:
+
+    def __init__(self, size: int):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex: list[Vertex] = [None] * size
+
+    def AddVertex(self, v):
+        index_of_empty = self._get_empty()
+        if index_of_empty is None:
+            return
+
+        self.vertex[index_of_empty] = Vertex(v)
+
+    def RemoveVertex(self, v: int):
+        if not self._are_indexies_ok(v):
+            return
+
+        for i in range(len(self.m_adjacency)):
+            self.m_adjacency[v][i] = 0
+            self.m_adjacency[i][v] = 0
+
+        self.vertex[v] = None
+
+    def IsEdge(self, v1: int, v2: int):
+        if not self._are_indexies_ok(v1, v2):
+            return None
+
+        return self.m_adjacency[v1][v2] > 0
+
+    def AddEdge(self, v1: int, v2: int):
+        if not self._are_indexies_ok(v1, v2):
+            return None
+
+        if self.vertex[v1] is None or self.vertex[v2] is None:
+            return
+
+        self.m_adjacency[v1][v2] += 1
+        if v1 != v2:
+            self.m_adjacency[v2][v1] += 1
+
+    def RemoveEdge(self, v1: int, v2: int):
+        if not self._are_indexies_ok(v1, v2):
+            return None
+
+        if self.m_adjacency[v1][v2] > 0:
+            self.m_adjacency[v1][v2] -= 1
+            if v1 != v2:
+                self.m_adjacency[v2][v1] -= 1
+
+    def DepthFirstSearch(self, VFrom: int, VTo: int):
+        for v in self.vertex:
+            if v is not None:
+                v.Hit = False
+
+        if not self._are_indexies_ok(VFrom, VTo):
+            return []
+
+        full_path = self._depth_first_search(VFrom, VTo, [])
+
+        return full_path
+
+    def BreadthFirstSearch(self, VFrom: int, VTo: int):
+        if not isinstance(VFrom, int) or not isinstance(VTo, int):
+            return None
+        if not self._are_indexies_in_range(VFrom, VTo):
+            return None
+        if VFrom == VTo:
+            return []
+
+        self.vertex[VFrom].Hit = True
+        result = self._breadth_first_search(VFrom, VTo, [VFrom])
+
+        self._unhit()
+
+        return result
+
+    def _breadth_first_search(self, currV: int, VTo: int, q: list[int]):
+        q.pop(0)
+
+        unvisited_related = self._get_unvisited_related(currV)
+
+        for v in unvisited_related:
+            if v == VTo:
+                return [self.vertex[currV], self.vertex[v]]
+            self.vertex[v].Hit = True
+            q.append(v)
+
+        if len(q) == 0:
+            return []
+
+        return [self.vertex[currV]] + self._breadth_first_search(q[0], VTo, q)
+
+    def _get_unvisited_related(self, vertex_index: int):
+        if vertex_index is None:
+            return None
+        if vertex_index >= len(self.vertex):
+            return None
+        if self.vertex[vertex_index] is None:
+            return None
+
+        related = self._get_related_vertexes(vertex_index)
+
+        return [v for v in related if self.vertex[v].Hit is False]
+
+    def _unhit(self):
+        for v in self.vertex:
+            v.Hit = False
+
+    def _get_related_vertexes(self, vertex_index: int):
+        relate = [i for i in range(len(self.m_adjacency[vertex_index])) if
+                  self.m_adjacency[vertex_index][i] >= 1]
+
+        return relate
+
+    def _depth_first_search(self, currV: int, VTo: int, path: list[int]):
+        self.vertex[currV].Hit = True
+        path.append(self.vertex[currV])
+
+        related = self._get_related_vertexes(currV)
+
+        if VTo in related:
+            self.vertex[VTo].Hit = True
+            path.append(self.vertex[VTo])
+            return path
+
+        path_ = path[:]
+
+        for v in related:
+            if self.vertex[v].Hit is True:
+                continue
+            path_ = (self._depth_first_search(v, VTo, path_))
+
+            if self.vertex.index(path_[-1]) == VTo:
+                return path_
+
+        path.pop()
+
+        return path
+
+    def _get_empty(self):
+        for i in range(len(self.vertex)):
+            if self.vertex[i] is None:
+                return i
+        return None
+
+    def _are_indexies_ok(self, *indexies: int):
+        for i in indexies:
+            if not isinstance(i, int):
+                return False
+        if not self._are_indexies_in_range(*indexies):
+            return False
+        if not self._are_there_vertexes(*indexies):
+            return False
+        return True
+
+    def _are_indexies_in_range(self, *indexies: int):
+        """Проверяет, умещаются ли индексы в размер графа."""
+        for i in indexies:
+            if i > len(self.vertex) - 1:
+                return False
+        return True
+
+    def _are_there_vertexes(self, *indexies: int):
+        """Проверяет, сушествуют ли вершины."""
+        for i in indexies:
+            if self.vertex[i] is None:
+                return False
+        return True
---
+++ BFSGraph/test_bfs_graph.py
@@ -0,0 +1,478 @@
+from bfs_graph import *
+
+
+# Test _get_unvisited_related
+
+# Некорректные входные данные могут быть такими:
+# 1) Вершины вне диапазона графа
+# 2) Вершина в диапазоне, но несуществует
+# 3) Передается None
+# 4) Передается неверный тип (такое не возможно при моей эксплуатации
+#    функции, а также в тестовой среде, так что не буду тестировать эту
+#    ситуацию)
+def test_get_unvis_related_input_out_of_range():
+    g = SimpleGraph(3)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+
+    assert g._get_unvisited_related(3) is None
+
+
+def test_get_unvis_related_doesnt_exist():
+    g = SimpleGraph(3)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+
+    assert g._get_unvisited_related(2) is None
+
+
+def test_get_unvis_related_none():
+    g = SimpleGraph(3)
+    g.AddVertex(0)
+    g.AddVertex(1)
+    assert g._get_unvisited_related(None) is None
+
+# После того, как некорректные данные были отсеяны, можно обособиться и
+# считать, что в функцию поступают лишь корректные данные. При работе с
+# данными (матрицей компетенции), следует рассмотреть следующие
+# ситуации:
+# У вершины 0, 1, несколько смежных вершин;
+# Граф целиком заполнен и не заполнен целиком;
+# Смежные вершины могут быть посещены, а могут быть и не посещены;
+# У вершины могут быть петли. 1 или несколько
+# У вершины может быть несколько ребер со смежной вершиной
+
+
+# Full graph
+
+def test_get_unvis_related_0_related():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    assert g._get_unvisited_related(3) == []
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_1_related():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 0)
+
+    assert g._get_unvisited_related(3) == [0]
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_few_related():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 0)
+    g.AddEdge(3, 2)
+
+    assert g._get_unvisited_related(3) == [0, 2]
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == [2]
+
+    g.vertex[2].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+# Not full graph
+
+def test_get_unvis_related_0_related_not_full():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    assert g._get_unvisited_related(3) == []
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_1_related_not_full():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 0)
+
+    assert g._get_unvisited_related(3) == [0]
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_few_related_not_full():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 0)
+    g.AddEdge(3, 2)
+
+    assert g._get_unvisited_related(3) == [0, 2]
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == [2]
+
+    g.vertex[2].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_loops():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 3)
+
+    assert g._get_unvisited_related(3) == [3]
+
+    g.AddEdge(3, 3)
+
+    assert g._get_unvisited_related(3) == [3]
+
+    g.vertex[3].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+def test_get_unvis_related_few_edges():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 2)
+
+    g.AddEdge(3, 0)
+    g.AddEdge(0, 3)
+    g.AddEdge(3, 0)
+    g.AddEdge(0, 3)
+
+    assert g._get_unvisited_related(3) == [0]
+
+    g.vertex[0].Hit = True
+
+    assert g._get_unvisited_related(3) == []
+
+
+# Test BFS
+
+"""
+Мысли о входных данных: На вход поступают два индекса вершин, очередь и
+стек.
+
+Возможная некорректность входных данных:
+
+1) Индексы за пределами графа. Алгоритм обработает такую ситуацию. Но
+    для этого ему прийдется обойти весь граф;
+2) Индексы совпадают. В случае, если вершина имеет петлю- алгоритм решит
+   задачу. Но если петли нет- пройдет весь граф и не обнаружит пути.
+   Поскольку задача поиска пути из вершины_один в вершину_один является
+   абсурдом- не будем об этом задумываться и будем выплевывать пустой
+   список.
+3) Индексы не принадлежат числовому типу (на самом деле стоило такую же
+   проверку сделать и для предыдущей функции. Но ладно уж, прощу сам
+   себе эту оплошность, но на будущее запомню);
+4) Очередь может быть не пустой с самого начала. (Изложил этот случай в
+   python jokes. Суть в том, что происходит замыкание и очередь может
+   содержать объекты оставшиеся от прошлого запуска);
+5) Стек может быть не пустым;
+
+Чтобы избежать этого- при вызове функции поиска из "главной" функции
+поиска, будем передавать пустые списки.
+
+6) Неявные входные данные: не все узлы могут оказаться нетронутыми;
+"""
+
+
+def test_bfs_indexes_out_of_range():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    assert g.BreadthFirstSearch(0, 4) is None
+    assert g.BreadthFirstSearch(4, 0) is None
+    assert g.BreadthFirstSearch(4, 7) is None
+
+
+def test_bfs_same_verticies():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    assert g.BreadthFirstSearch(0, 0) == []
+
+    g.AddEdge(0, 0)
+
+    assert g.BreadthFirstSearch(0, 0) == []
+
+
+def test_bfs_wrong_type():
+    g = SimpleGraph(4)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+
+    assert g.BreadthFirstSearch('0', 1) is None
+    assert g.BreadthFirstSearch(0, '1') is None
+    assert g.BreadthFirstSearch(None, None) is None
+
+
+def test_bfs_are_hits_false():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+    g.AddVertex(4)
+    g.AddVertex(5)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 3)
+    g.AddEdge(4, 1)
+    g.AddEdge(4, 5)
+
+    verticies = g.vertex
+
+    g.BreadthFirstSearch(0, 5)
+
+    for v in verticies:
+        assert v.Hit is False
+
+    g.BreadthFirstSearch(1, 2)
+
+    for v in verticies:
+        assert v.Hit is False
+
+    g.BreadthFirstSearch(4, 4)
+
+    for v in verticies:
+        assert v.Hit is False
+
+
+"""
+Тесты на работу с данными.
+"""
+
+
+def test_bfs_simple():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+    g.AddVertex(4)
+    g.AddVertex(5)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(1, 2)
+    g.AddEdge(2, 3)
+    g.AddEdge(3, 4)
+    g.AddEdge(4, 5)
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[2],
+        g.vertex[3],
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+
+"""
+Возможно несколько ситуаций, которые следует обработать:
+- Петли
+- Циклы
+- Множественные ребра между двумя нодами
+- Несвязный граф, в котором стартовая нода и искомая ноды находятся в
+разных подграфах
+"""
+
+
+def test_bfs_loops():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+    g.AddVertex(4)
+    g.AddVertex(5)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(1, 2)
+    g.AddEdge(2, 3)
+    g.AddEdge(3, 4)
+    g.AddEdge(4, 5)
+
+    g.AddEdge(0, 0)
+    g.AddEdge(0, 0)
+
+    g.AddEdge(1, 1)
+    g.AddEdge(1, 1)
+
+    g.AddEdge(2, 2)
+    g.AddEdge(2, 2)
+
+    g.AddEdge(3, 3)
+    g.AddEdge(3, 3)
+
+    g.AddEdge(4, 4)
+    g.AddEdge(4, 4)
+
+    g.AddEdge(5, 5)
+    g.AddEdge(5, 5)
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[2],
+        g.vertex[3],
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+
+def test_bfs_cycles_1():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+    g.AddVertex(4)
+    g.AddVertex(5)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(1, 2)
+    g.AddEdge(2, 3)
+    g.AddEdge(3, 4)
+    g.AddEdge(4, 5)
+
+    g.AddEdge(1, 0)
+    g.AddEdge(2, 0)
+    g.AddEdge(3, 0)
+    g.AddEdge(4, 0)
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[2],
+        g.vertex[3],
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+
+def test_bfs_cycles_2():
+    g = SimpleGraph(6)
+
+    g.AddVertex(0)
+    g.AddVertex(1)
+    g.AddVertex(2)
+    g.AddVertex(3)
+    g.AddVertex(4)
+    g.AddVertex(5)
+
+    g.AddEdge(0, 1)
+    g.AddEdge(1, 2)
+    g.AddEdge(2, 3)
+    g.AddEdge(3, 4)
+    g.AddEdge(4, 1)
+    g.AddEdge(3, 5)
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[2],
+        g.vertex[3],
+        g.vertex[5],
+    ]
